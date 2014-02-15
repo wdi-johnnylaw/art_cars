8fbbfd951aedf406df021802a502bd30c05796bb
John Lawrence <johnonrails@gmail.com>
2014-02-15 11:57:58 -0800

    Set up password hashing with confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,23 @@ class User
   field :username, type: String
   field :hashed_password, type: String
 
-  def password=(arg)
-  	salt = BCrypt::Engine.generate_salt
-  	self.hashed_password = 
-  		BCrypt::Engine.hash_secret arg, salt
-  end
+  attr_accessor :password, :password_confirmation
+
+  before_save :update_password
+
+  validates :username, uniqueness: true
+  validates :password, confirmation: true
 
   def authenticates_with_password?(pwd)
   	BCrypt::Password.new(hashed_password) == pwd
+  end
+
+  private
+
+  def update_password
+    if password.present?
+      salt = BCrypt::Engine.generate_salt
+      self.hashed_password = BCrypt::Engine.hash_secret password, salt
+    end
   end
 end
